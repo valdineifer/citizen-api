28f481505efe3ddacf87d3b0a7ec33a4c9ade44a
Valdinei Ferreira <valdineifer@outlook.com>
2018-11-17 04:54:22 -0300

    Update .gitignore and config file

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { DB_USER, DB_PASS } from './.env';
+const config = require('./config.json');
 
 const express = require('express')
 const mongoose = require('mongoose')
@@ -9,7 +9,7 @@ const app = express() // Creates an express aplication
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(`mongodb://${DB_USER}:${DB_PASS}@ds111244.mlab.com:11244/citizen-api`, { useNewUrlParser: true })
+mongoose.connect(`mongodb://${config.DB_USER}:${config.DB_PASS}@ds111244.mlab.com:11244/citizen-api`, { useNewUrlParser: true })
 
 requireDir('./src/models') // Require every file on this directory
 
