03be8d4bdae5cf98a3635a8599b0dcbb110cacf3
Valdinei Ferreira <valdineifer@outlook.com>
2018-11-17 23:40:40 -0300

    Fix port settings

--- server.js
+++ server.js
@@ -15,4 +15,6 @@ requireDir('./src/models') // Require every file on this directory
 
 app.use('/api', require('./src/routes'))
 
-app.listen(3001)
+app.listen(process.env.PORT || 3000, () => {
+	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+})
