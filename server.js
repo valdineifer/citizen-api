668609e845048d2d379dbba4ac3d411e49b9b829
Valdinei Ferreira <valdineifer@outlook.com>
2019-07-17 04:55:43 -0300

    Add semistandardjs and PORT variable to .env

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const requireDir = require('require-dir')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
 
-const app = express() // Creates an express aplication
-app.use(express.json())
-app.use(cors())
+const app = express(); // Creates an express aplication
+app.use(express.json());
+app.use(cors());
 
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds111244.mlab.com:11244/citizen-api`, { useNewUrlParser: true })
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds111244.mlab.com:11244/citizen-api`, { useNewUrlParser: true });
 
-requireDir('./src/models') // Require every file on this directory
+requireDir('./src/models'); // Require every file on this directory
 
-app.use('/api', require('./src/routes'))
+app.use('/api', require('./src/routes'));
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
