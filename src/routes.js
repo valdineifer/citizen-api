6b487c4aecfa5be786b37c8088731c15114d3ddd
Valdinei Ferreira <valdineifer@outlook.com>
2019-07-18 04:23:53 -0300

    Finish API authentication with JWT
    * add bcryptjs and jwt package
    * add routes, as well as controller and middleware
    * create files for auth: model, controller, config file

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const routes = express.Router()
 const CitizenController = require('./controllers/CitizenController')
+const AuthController = require('./controllers/AuthController')
+const AuthMiddleware = require('./middlewares/auth')
 
 routes.get('/citizen', CitizenController.index)
-routes.post('/citizen', CitizenController.store)
+routes.post('/citizen', AuthMiddleware, CitizenController.store)
 routes.get('/citizen/:cpf', CitizenController.show)
-routes.put('/citizen/:cpf', CitizenController.update)
-routes.delete('/citizen/:cpf', CitizenController.destroy)
+routes.put('/citizen/:cpf', AuthMiddleware, CitizenController.update)
+routes.delete('/citizen/:cpf', AuthMiddleware, CitizenController.destroy)
+
+routes.post('/auth/register', AuthController.register)
+routes.post('/auth/authenticate', AuthController.login)
 
 module.exports = routes
